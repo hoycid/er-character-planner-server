a5fa2f91b0268748a47de076d1211355ca5f1fc3
hoycid <quilangcidrex@gmail.com>
2024-10-20 19:30:22 +1300

    commented out cors

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-const corsOptions = {
-  origin: [
-    "https://er-character-planner-teal.vercel.app",
-    "http://localhost:3000",
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-};
+// const corsOptions = {
+//   origin: [
+//     "https://er-character-planner-teal.vercel.app",
+//     "http://localhost:3000",
+//   ],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
